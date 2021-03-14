497e0c8f3305da2abc482fa63374269323811431
Z-Slava <zhechievviacheslav@gmail.com>
2021-03-14 16:09:00 +0200

    fix: adjust test folder stracture

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'fs';
-import genDiff  from '../../src/index.js';
+import genDiff  from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- __tests__/parsers/index.test.js
+++ __tests__/parsers/index.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, join, extname } from 'path';
 import { readFileSync } from 'fs';
-import getParsedFile from '../../../src/parsers/index.js';
+import getParsedFile from '../../src/parsers/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
